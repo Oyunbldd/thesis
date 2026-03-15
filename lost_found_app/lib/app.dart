32b2aa24082436db1646c91480823e074a15e16e
Oyunbldd Ganbldd <oyunbldd@Oyunbldds-MacBook-Pro.local>
2026-03-15 21:27:04 +0100

    feat:implemented lostitemview (#issue29)

--- lost_found_app/lib/app.dart
+++ lost_found_app/lib/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'utils/app_routes.dart';
 import 'utils/app_theme.dart';
 import 'views/login/login_view.dart';
-import 'views/home/home_view.dart';
+import 'views/lost/lost_items_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       initialRoute: AppRoutes.login,
       routes: {
         AppRoutes.login: (context) => const LoginView(),
-        AppRoutes.home: (context) => const HomeView(),
+        AppRoutes.home: (context) => const LostItemsView(),
       },
     );
   }
